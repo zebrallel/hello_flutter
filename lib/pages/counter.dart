04e9db2c45a4194380376ecf7a8776b5689e29ea
xuejian.xu01 <xuejian.xu01@liulishuo.com>
2019-09-24 20:59:29 +0800

    update

--- lib/pages/counter.dart
+++ lib/pages/counter.dart
@@ -9,10 +9,62 @@ class CounterPage extends StatefulWidget {
 }
 
 class _CounterState extends State<CounterPage> {
+  int _count;
+
+  @override
+  void initState() {
+    super.initState();
+
+    this._count = 0;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Layout(
-      child: Text('this is counter page'),
-    );
+        child: Center(
+            child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text('Count: ${this._count}'),
+        RaisedButton(
+          child: Text('add count'),
+          onPressed: () {
+            setState(() {
+              this._count++;
+            });
+          },
+        )
+      ],
+    )));
+  }
+
+  @override
+  void didUpdateWidget(CounterPage oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    print("didUpdateWidget");
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+    print("deactive");
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print("dispose");
+  }
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    print("reassemble");
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print("didChangeDependencies");
   }
 }
